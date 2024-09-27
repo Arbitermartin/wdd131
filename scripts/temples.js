2d71b87f6dcdc3ed47bcc6783cde4c7b94747537
Arbitermartin <arbitermartin04@gmail.com>
2024-09-27 10:46:02 +0300

    add changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,11 +2,5 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.querySelector('#menu-button');
-    const nav = document.querySelector('nav ul');
+ 
 
-    menuButton.addEventListener('click', () => {
-        nav.classList.toggle('open');
-    });
-});
