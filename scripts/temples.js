443109f412c04f74c0375c4c56f68b3c26905bfa
Arbitermartin <arbitermartin04@gmail.com>
2024-09-27 12:03:49 +0300

    add changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,12 @@
-// Set current year and last modified date
-document.getElementById('year').textContent = new Date().getFullYear();
+const now = new Date();
+const year = now.getFullYear();
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
 document.getElementById('lastModified').textContent = document.lastModified;
+document.getElementById('currentYear').textContent = year;
 
- 
-
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
